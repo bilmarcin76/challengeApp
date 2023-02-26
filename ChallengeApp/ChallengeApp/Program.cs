7c2c07edc37ac2530a8f2313768d5d6beb1f5dff
marcin <bilmarcin76@gmail.com>
2023-02-26 16:25:52 +0100

    problemy

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] grades = new int[5];
+﻿int[] grades = new int[365];
 string[] daysofweeks = new string[7];
 daysofweeks[0] = "poniedziałek";
 daysofweeks[1] = "wtorek";
@@ -7,4 +7,13 @@ daysofweeks[3] = "czwartek";
 daysofweeks[4] = "piątek";
 daysofweeks[5] = "sobota";
 daysofweeks[6] = "niedziela";
-Console.WriteLine(daysofweeks[5]);
+
+//Console.WriteLine(daysofweeks[2]);
+
+//TWORZENIE PĘTLI "FOR"
+
+for(var i=0; i<= 100; i++) ;
+
+{
+    Console.WriteLine(i);
+}
